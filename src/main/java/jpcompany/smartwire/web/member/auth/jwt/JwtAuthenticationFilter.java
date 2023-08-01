a17ce793b4b99093e6f9bb8a15c1420ce4abc1f2
jjp <wjsdj2008@gmail.com>
2023-08-01 21:02:14 +0900

    api 기계 정보 보내기 테스트

--- src/main/java/jpcompany/smartwire/web/member/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/jpcompany/smartwire/web/member/auth/jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package jpcompany.smartwire.web.member.auth.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jpcompany.smartwire.web.machine.dto.MachineDto;
 import jpcompany.smartwire.web.machine.repository.MachineRepositoryJdbcTemplate;
@@ -86,12 +87,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         List<MachineDto> machineList = machineRepository.findAll(principalDetails.getMember().getId());
         machineList.sort(Comparator.comparingInt(MachineDto::getSequence));
+        ObjectMapper objectMapper = new ObjectMapper();
+        String machineListJson;
+        try {
+            machineListJson = objectMapper.writeValueAsString(machineList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert machineList to JSON", e);
+        }
+
+
+        log.info("기계 리스트 api 로 보내기={}", machineListJson);
 
         String jwtToken = JWT.create()
                 .withSubject(principalDetails.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
                 .withClaim("username", principalDetails.getMember().getLoginId())
-                .withClaim("machineList", machineList)
+                .withClaim("machineList", machineListJson)
                 .sign(Algorithm.HMAC512(JwtProperties.SECRET));
         log.info("JWT 토큰={}", jwtToken);
 
