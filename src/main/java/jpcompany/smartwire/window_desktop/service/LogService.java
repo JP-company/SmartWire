3774d90e75c5bc66f73331d806b0b8e462441794
jjp <wjsdj2008@gmail.com>
2023-09-23 13:45:32 +0900

    LogService 수정2

--- src/main/java/jpcompany/smartwire/window_desktop/service/LogService.java
+++ src/main/java/jpcompany/smartwire/window_desktop/service/LogService.java
@@ -3,8 +3,6 @@ package jpcompany.smartwire.window_desktop.service;
 import jpcompany.smartwire.domain.Date;
 import jpcompany.smartwire.domain.Log;
 import jpcompany.smartwire.domain.Process;
-import jpcompany.smartwire.web.machine.repository.MachineRepositoryJdbcTemplate;
-import jpcompany.smartwire.web.member.repository.MemberRepository;
 import jpcompany.smartwire.window_desktop.dto.LogSaveDto;
 import jpcompany.smartwire.window_desktop.repository.LogReceiverJdbcTemplateRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
@@ -22,18 +19,11 @@ import java.time.format.DateTimeFormatter;
 @Transactional
 public class LogService {
     private final LogReceiverJdbcTemplateRepository logReceiverRepository;
-    private final MemberRepository memberRepository;
-    private final MachineRepositoryJdbcTemplate machineRepository;
 
     public void saveLog(LogSaveDto logSaveDto) {
-        // 윈도우한테 받은 데이터에서 회원 정보 추출
-//        Member member = memberRepository.findByLoginId(logSaveDto.getLoginId()).get();
-
-        // 회원 id, 기계 이름을 가지고 기계 id 추출
-//        Integer machineId = machineRepository.findByMemberIdNMachineName(member.getId(), logSaveDto.getMachineName()).get();
         Integer machineId = logSaveDto.getMachineId();
 
-        // 기계 id를 가지고 최근 날짜 id 추출
+        // 받은 로그에서 기계 id를 가지고 최근 날짜 id 추출
         Date recentDateDto = logReceiverRepository.findRecentDateByMachineId(machineId).orElse(null);
 
         Integer machineDateId;
@@ -42,28 +32,27 @@ public class LogService {
         if (recentDateDto != null) {  // 최근날짜가 있으면
             machineDateId = recentDateDto.getId();
             recentDate = recentDateDto.getDate();
-            log.info("machineDateId 처음에={}", machineDateId);
+
+            // 데이터에 날짜가 있으면 새로운 날짜 생성, 없으면 이전과 같은 날짜
+            if (logSaveDto.getDate() != null) {
+                Date date = new Date();
+                date.setDate(logSaveDto.getDate());
+                date.setMachineId(machineId);
+                log.info("날짜={}", date);
+
+                Date date1 = logReceiverRepository.saveDate(date);
+                machineDateId = date1.getId();
+                recentDate = date1.getDate();
+                log.info("기계 날짜 id={}", machineDateId);
+            }
         } else {  // 최근날짜가 없으면
             recentDate = LocalDate.now();
             Date date = new Date();
             date.setDate(recentDate);
             date.setMachineId(machineId);
             log.info("날짜={}", date);
-            Date date1 = logReceiverRepository.saveDate(date);
-            machineDateId = date1.getId();
-        }
-
-        // 데이터에 날짜가 있으면 새로운 날짜 생성, 없으면 이전과 같은 날짜
-        if (logSaveDto.getDate() != null) {
-            Date date = new Date();
-            date.setDate(logSaveDto.getDate());
-            date.setMachineId(machineId);
-            log.info("날짜={}", date);
-
-            Date date1 = logReceiverRepository.saveDate(date);
-            machineDateId = date1.getId();
-            recentDate = date1.getDate();
-            log.info("기계 날짜 id={}", machineDateId);
+            Date newDate = logReceiverRepository.saveDate(date); // DB Date 테이블에 업로드
+            machineDateId = newDate.getId();
         }
 
         // 기계 날짜 id를 가지고 최근 작업 id 추출
