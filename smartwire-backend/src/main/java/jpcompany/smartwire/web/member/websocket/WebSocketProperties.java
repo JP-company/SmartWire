173b4fe91529a06444c48bf8914af843ede8be7a
JP-company <wjsdj2008@gmail.com>
2023-11-16 18:43:03 +0900

    Refactor: [수정] 백엔드 서버

--- smartwire-backend/src/main/java/jpcompany/smartwire/web/member/websocket/WebSocketProperties.java
+++
@@ -1,6 +0,0 @@
-package jpcompany.smartwire.web.member.websocket;
-
-public interface WebSocketProperties {
-    String URL = "http://localhost:8080";
-//    String URL = "https://smartwire-backend-f39394ac6218.herokuapp.com";
-}
